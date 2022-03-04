4c716e66761072d4a2954c8916374c585403361c
Rodrigo Baba <rodrigonuciatelli@gmail.com>
2022-03-04 17:30:44 -0300

    @RodrigoBaba [Adicionando Edit e Delete no Tema e Postagem]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -13,6 +13,16 @@ export class AuthService {
   constructor(
     private http: HttpClient
   ) { }
+  
+  token = {
+    headers: new HttpHeaders().set("Authorization", environment.token)
+  }
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set("Authorization", environment.token)
+    };
+  }
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
     return this.http.post<UsuarioLogin>("https://blog-pessoal-generation-brasil.herokuapp.com/usuarios/logar", usuarioLogin);
@@ -20,6 +30,14 @@ export class AuthService {
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>("https://blog-pessoal-generation-brasil.herokuapp.com/usuarios/cadastrar", usuario);
+  }
+
+  atualizar(usuario: Usuario): Observable<Usuario>{
+    return this.http.put<Usuario>("https://blog-pessoal-generation-brasil.herokuapp.com/usuarios/atualizar", usuario, this.token);
+  }
+
+  getByIdUser(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://blog-pessoal-generation-brasil.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
